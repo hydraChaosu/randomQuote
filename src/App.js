4ffcfc69bfe9ca3024fc6971db9407e3eb998557
hydraChaosu <abmallim@gmail.com>
2019-08-17 15:49:36 +0200

    fISHEd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Button, Jumbotron, Container } from "react-bootstrap";
+// import { Button } from "reactstrap";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [text, setText] = useState("Example text");
+  const [author, setAuthor] = useState("Example author");
+  useEffect(() => {
+    getNewQuote();
+  }, []);
+
+  const getNewQuote = () => {
+    axios.get("https://api.quotable.io/random").then(res => {
+      setText(res.data.content);
+      setAuthor(res.data.author);
+    });
+  };
+
+  const tweetQuote = () => {
+    window.open(
+      `http://twitter.com/intent/tweet?text="${text}" ${author}`,
+      "_blank"
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper" id="quote-box">
+      <Jumbotron className="d-flex flex-column justify-content-between">
+        <div className="text-wrapper">
+          <p id="text" className="lead">
+            "{text}"
+          </p>
+          <p id="author" className="text-right">
+            {author}
+          </p>
+        </div>
+        <div className="btn-wrapper">
+          <a
+            className="btn btn-success"
+            id="tweet-quote"
+            // rel="noopener noreferrer"
+            onClick={() => tweetQuote()}
+            // href={`http://twitter.com/intent/tweet?text="${text}" ${author}`}
+          >
+            Tweet Quote
+          </a>
+          <Button id="new-quote" onClick={getNewQuote}>
+            New Quote
+          </Button>
+        </div>
+      </Jumbotron>
     </div>
   );
 }
